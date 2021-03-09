72e1e2b04a8713cff653ae451207e629706fb673
Divya C D <62093093+DivyaBharath-prog@users.noreply.github.com>
2021-03-09 17:06:36 +0530

    Update Program-4.c

--- Program-4.c
+++ Program-4.c
@@ -1,11 +1,8 @@
-// Calculate the average
-//Input: 10 20 30
-//Output: 20
-
-
+//Program to find the average
+//Input : 10 20 30 
+   // Output :20
 
 #include <stdio.h>
-#include <stdlib.h>
 struct math {
     float num1;
     float num2;
@@ -19,8 +16,7 @@ int main()
     struct math enternum;
     struct math avg;
     printf("Enter a number1,number2,number3:\n");
-    scanf("%f %f %f ", &enternum.num1,&enternum.num2,&enternum.num3);
-     avg.average = (enternum.num1 + enternum.num2 + enternum.num3)/3;
-     printf("The average is: %.2f",avg.average);
+    scanf("%f%f%f", &enternum.num1,&enternum.num2,&enternum.num3);
+     //compute and print the average
     return 0;
 } 
